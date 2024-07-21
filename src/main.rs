06acbe6da80861c8a72990a7eaea6a83fb346393
Abdürrahim YILMAZ <84680978+yilmaz08@users.noreply.github.com>
2024-07-21 14:43:45 +0300

    Save to file

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,12 @@ struct Args {
     port: u16,
     #[arg(help = "Content", short, long)]
     content: String,
+    #[arg(help = "Output File", short, long)]
+    output: Option<String>,
     #[arg(help = "Lossy UTF-8 Conversion", long)]
     lossy: bool,
     #[arg(help = "Force", short, long)]
     force: bool,
-
-    // TODO: Implement Output
-    #[arg(help = "Output File", short, long)]
-    output: Option<String>,
 }
 
 fn normalize_text(content: String) -> String {
@@ -39,7 +37,25 @@ fn print_content(content: Vec<u8>, lossy: bool) {
     }
     match String::from_utf8(content) {
         Ok(s) => println!("{}", s),
-        Err(_e) => println!("Content is not valid UTF-8. Use --lossy to print it anyways.")
+        Err(_e) => println!("Content is not valid UTF-8. Use --lossy to print it anyways or save it to a file.")
+    }
+}
+
+fn get_user_input(prompt: String) -> String {
+    print!("{}: ", prompt);
+    io::stdout().flush().expect("Failed to flush stdout");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    return input;
+}
+
+fn write_to_file(content: Vec<u8>, file_path: String, create: bool) -> std::io::Result<()> {
+    if create {
+        let mut file = std::fs::File::create(file_path)?;
+        return file.write_all(&content);
+    } else {
+        let mut file = std::fs::OpenOptions::new().append(true).open(file_path)?;
+        return file.write_all(&content);
     }
 }
 
@@ -79,8 +95,22 @@ fn main() -> std::io::Result<()> {
         return Ok(());
     }
 
-    // TODO: Save content to file
-    println!("Save to file - TODO");
+    // Save content to file
+    let file_path = args.output.unwrap();
+    if std::path::Path::new(&file_path).exists() {
+        if args.force {
+            let _ = write_to_file(content, file_path, true);
+            return Ok(());
+        }
+        let inp = get_user_input("File already exists! (O)verride/(A)ppend/(C)ancel".to_string());
+
+        match inp.trim().to_uppercase().as_str() {
+            "O" => { let _ = write_to_file(content, file_path, true); }
+            "A" => { let _ = write_to_file(content, file_path, false); } 
+            _ => { println!("Cancelled"); }
+        }
+        
+    } else { write_to_file(content, file_path, true)?; } // Create
 
     return Ok(());
 }
